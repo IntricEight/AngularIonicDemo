43822c6a01aee221f6beeb75a30437eef564d9ca
Collin Bowdoin <70925258+IntricEight@users.noreply.github.com>
2024-06-24 22:59:35 -0700

    Third tab demonstation, showing the ability to change CSS features of components dynamically using buttons

--- AngularIonicDemo/src/app/tab3/tab3.page.ts
+++ AngularIonicDemo/src/app/tab3/tab3.page.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NgStyle } from '@angular/common'
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [NgStyle, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton, IonLabel],
 })
 export class Tab3Page {
+  fontSize: number = 1;
+  textColor: string = 'black';
+  backgroundColor: string = 'white';
+  
   constructor() {}
+
+  setSize(size: number) {
+    this.fontSize = size;
+  }
+
+  setTextColor(color: string) {
+    this.textColor = color;
+  }
+
+  setBackgroundColor(color: string) {
+    this.backgroundColor = color;
+  }
 }
